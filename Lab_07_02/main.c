ecb270913c4ba5693dd0eb52289c73fdadee8fd0
DavideCarletto <carlettodavide@gmail.com>
2022-05-28 16:56:57 +0200

    primo commit

--- Lab_07_02/main.c
+++ Lab_07_02/main.c
@@ -1,6 +1,121 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define MAX 100
+
+void leggiFile(FILE *fin);
+void stampaVettore(int v[MAX], int n);
+void copiaVettore(int v[MAX], int v_copia[MAX], int n);
+
+void insertionSort(int v[MAX], int n);
+void selectionSort(int v[MAX], int n);
+void shellSort(int v[MAX], int n);
+
+    int main() {
+    FILE *fin, *fin2;
+
+    if((fin = fopen("../sort.txt","r"))==NULL){
+        printf("Error opening file");
+        return 1;
+    }
+
+    if((fin2 = fopen("../sort1.txt","r"))==NULL){
+        printf("Error opening file");
+        return 2;
+    }
+
+    leggiFile(fin);
+
     return 0;
+}
+
+void leggiFile(FILE *fin){
+    int dim,n;
+    int v[MAX],v_copia[MAX];
+
+    fscanf(fin, "%d",&dim);
+
+    while(!feof(fin)){
+        fscanf(fin,"%d",&n);
+        for(int i=0; i<n; i++){
+            fscanf(fin, "%d",&v[i]);
+        }
+        copiaVettore(v,v_copia, n);
+        stampaVettore(v, n);
+
+        insertionSort(v_copia,n);
+        printf("Vettore orinato con insertion sort:");
+        stampaVettore(v_copia, n);
+
+        copiaVettore(v,v_copia, n);
+        selectionSort(v_copia,n);
+        printf("Vettore orinato con selection sort:");
+        stampaVettore(v_copia, n);
+
+        copiaVettore(v,v_copia, n);
+        shellSort(v_copia,n);
+        printf("Vettore orinato con shell sort:");
+        stampaVettore(v_copia, n);
+        printf("\n");
+    }
+
+}
+
+void stampaVettore(int v[MAX], int n){
+    for(int i=0; i< n;i++){
+        printf(" %d ",v[i]);
+    }
+    printf("\n");
+}
+
+void copiaVettore(int v[MAX], int v_copia[MAX], int n){
+    for(int i=0; i< n; i++){
+        v_copia[i]= v[i];
+    }
+}
+void insertionSort(int A[], int N) {
+    int i, j, l=0, r=N-1, x;
+    for (i = l+1; i <= r; i++) {
+        x = A[i];
+        j = i - 1;
+        while (j >= l && x < A[j]){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = x;
+    }
+}
+
+void selectionSort(int A[], int N) {
+    int i, j, l=0, r=N-1, min;
+    int temp;
+    for (i = l; i < r; i++) {
+        min = i;
+        for (j = i+1; j <= r; j++)
+            if (A[j] < A[min])
+                min = j;
+        if (min != i) {
+            temp = A[i];
+            A[i] = A[min];
+            A[min] = temp; } }
+    return;
+
+    }
+
+
+void shellSort(int A[], int N) {
+    int i, j, x, l=0, r=N-1, h=1;
+    while (h < N/3)
+        h = 3*h+1;
+    while(h >= 1) {
+        for (i = l + h; i <= r; i++) {
+            j = i;
+            x = A[i];
+            while(j >= l + h && x < A[j-h]) {
+                A[j] = A[j-h];
+                j -=h;
+            }
+            A[j] = x;
+        }
+        h = h/3;
+    }
 }

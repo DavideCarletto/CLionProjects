ed9cb9dd999102a3787a19c7527f186a29046b03
DavideCarletto <102655420+DavideCarletto@users.noreply.github.com>
2022-11-06 23:15:42 +0100

    Update: aggiunto il CmakeList

---
+++ ASD/Lab_04_02/cmake-build-debug/CMakeFiles/Lab_04_02.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_04_02.dir/main.c.obj"
+  "CMakeFiles/Lab_04_02.dir/main.c.obj.d"
+  "Lab_04_02.exe"
+  "Lab_04_02.exe.manifest"
+  "Lab_04_02.pdb"
+  "libLab_04_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_04_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ASD/Lab_04_02/cmake-build-debug/CMakeFiles/Lab_04_02.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab_04_02.
+# This may be replaced when dependencies are built.
